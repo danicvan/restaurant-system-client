3ac82bcb29611a755ce2f806cdeca96c26278323
Daniel Evangelista <danicvan@hotmail.com>
2023-12-07 09:51:24 -0300

    Aditioned useState to change pages.

--- src/Components/Card/Card.jsx
+++ src/Components/Card/Card.jsx
@@ -1,17 +1,29 @@
-import React from "react";
 import { CardData } from "../CardData/CarData";
+import { useState } from "react";
+
+import Item from "../Item/Item";
 
 function Card() {
+    const [showItem, setShowItem] = useState(false);
+
+    const handleCardClick = () => {
+        setShowItem(!showItem);
+    };
+
     return (
         <>
-            {CardData.map((item, index) => {
-                return (
-                    <div key={index} className="card">
+            <div className={`card-container ${showItem? "hidden" : ""}`}>
+                {CardData.map((item, index) => (
+                    <div key={index} className="card" onClick={handleCardClick}>
                         <img src={item.image} alt={item.title} />
                         <span>{item.title}</span>
                     </div>
-                );
-            })}
+                ))}
+            </div>
+
+            <div className="item">
+                <Item visible={showItem} />
+            </div>
         </>
     );
 }
--- src/Components/Item/Item.jsx
+++ src/Components/Item/Item.jsx
@@ -1,9 +1,108 @@
-function Item() {
+import Navbar from "../Navbar/Navbar";
+import Image1 from "../../assets/image-1.png";
+import Image2 from "../../assets/image-2.png";
+
+function Item({ visible }) {
+  
+  if (!visible) return null;
+
   return (
     <>
-        div.section
+      <Navbar />
+
+      <div className="section">
+        <div className="section__combo">
+          <h3>Hot Dishes</h3>
+
+          <div className="section__description">
+            <h2>Oyster Dynamite</h2>
+            <p>Two 1/2 shells, shitake and spinach baked w/ dynamite sauce</p>
+            <h4>RM23.90 + RM1.20(SST) = </h4>
+            <h3>RM25.10</h3>
+            <h4>[166 Kcal]</h4>
+          </div>
+
+          <div className="section__image">
+            <img src={Image2} alt="" />
+          </div>
+
+          <div className="section__description">
+            <h2>Fried Calamari Salad</h2>
+            <p>Two 1/2 shells, shitake and spinach baked w/ dynamite sauce</p>
+            <h4>RM23.90 + RM1.20(SST) = </h4>
+            <h3>RM25.10</h3>
+            <h4>[166 Kcal]</h4>
+          </div>
+
+          <div className="section__image">
+            <img src={Image1} alt="" />
+          </div>
+        </div>
+
+        <div className="section__item">
+          <div className="section__description">
+            <h2>Shrimp Tempura</h2>
+            <p>5 tiger shrimp with tempura sauce</p>
+            <h4>RM23.90 + RM1.20(SST) = </h4>
+            <h3>RM25.10</h3>
+            <h4>[166 Kcal]</h4>
+          </div>
+
+          <div className="section__image">
+            <img src={Image1} alt="" />
+          </div>
+
+          <div className="section__description">
+            <h2>Shrimp Tempura</h2>
+            <p>5 tiger shrimp with tempura sauce</p>
+            <h4>RM23.90 + RM1.20(SST) = </h4>
+            <h3>RM25.10</h3>
+            <h4>[166 Kcal]</h4>
+          </div>
+
+          <div className="section__image">
+            <img src={Image1} alt="" />
+          </div>
+
+          <div className="section__description">
+            <h2>Shrimp Tempura</h2>
+            <p>5 tiger shrimp with tempura sauce</p>
+            <h4>RM23.90 + RM1.20(SST) = </h4>
+            <h3>RM25.10</h3>
+            <h4>[166 Kcal]</h4>
+          </div>
+
+          <div className="section__image">
+            <img src={Image1} alt="" />
+          </div>
+
+          <div className="section__description">
+            <h2>Shrimp Tempura</h2>
+            <p>5 tiger shrimp with tempura sauce</p>
+            <h4>RM23.90 + RM1.20(SST) = </h4>
+            <h3>RM25.10</h3>
+            <h4>[166 Kcal]</h4>
+          </div>
+
+          <div className="section__image">
+            <img src={Image1} alt="" />
+          </div>
+
+          <div className="section__description">
+            <h2>Shrimp Tempura</h2>
+            <p>5 tiger shrimp with tempura sauce</p>
+            <h4>RM23.90 + RM1.20(SST) = </h4>
+            <h3>RM25.10</h3>
+            <h4>[166 Kcal]</h4>
+          </div>
+
+          <div className="section__image">
+            <img src={Image1} alt="" />
+          </div>
+        </div>
+      </div>
     </>
-  )
+  );
 }
 
 export default Item;
