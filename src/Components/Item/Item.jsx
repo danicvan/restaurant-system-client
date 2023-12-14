458a5a147ebc17776b09b0567a6b8c4c4d517c64
Daniel Camilo <daniel.camilo@grupomfx.com>
2023-12-13 22:33:50 -0300

    chore: updating dinamic menu page

--- src/Components/Item/Item.jsx
+++ src/Components/Item/Item.jsx
@@ -1,11 +1,25 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Navbar from "../Navbar/Navbar";
-import Image1 from "../../assets/image-1.png";
-import Image2 from "../../assets/image-2.png";
-import Image3 from "../../assets/image-3.png";
 import Product from "../Product/Product";
 
 function Item({ visible }) {
+
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch("http://localhost:3000/products");
+        const data = await response.json();
+        setProducts(data);
+      } catch (error) {
+        console.error("Error fetching products data:", error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
   if (!visible) return null;
 
   const [showProduct, setShowProduct] = useState(false);
@@ -22,91 +36,16 @@ function Item({ visible }) {
         <div className="section__combo">
           <h3>Hot Dishes</h3>
 
-          <div className="section__description">
-            <h2>Oyster Dynamite</h2>
-            <p>Two 1/2 shells, shitake and spinach baked w/ dynamite sauce</p>
-            <h4>RM23.90 + RM1.20(SST) = </h4>
-            <h3>RM25.10</h3>
-            <h4>[166 Kcal]</h4>
-          </div>
-
-          <div className="section__image" onClick={handleProductClick}>
-            <img src={Image1} alt="" />
-          </div>
-
-          <div className="section__description">
-            <h2>Fried Calamari Salad</h2>
-            <p>Two 1/2 shells, shitake and spinach baked w/ dynamite sauce</p>
-            <h4>RM23.90 + RM1.20(SST) = </h4>
-            <h3>RM25.10</h3>
-            <h4>[166 Kcal]</h4>
-          </div>
-
-          <div className="section__image" onClick={handleProductClick}>
-            <img src={Image2} alt="" />
-          </div>
-        </div>
-
-        <div className="section__item">
-          <div className="section__description">
-            <h2>Shrimp Tempura</h2>
-            <p>5 tiger shrimp with tempura sauce</p>
-            <h4>RM23.90 + RM1.20(SST) = </h4>
-            <h3>RM25.10</h3>
-            <h4>[166 Kcal]</h4>
-          </div>
-
-          <div className="section__image" onClick={handleProductClick}>
-            <img src={Image3} alt="" />
-          </div>
-
-          <div className="section__description">
-            <h2>Shrimp Tempura</h2>
-            <p>5 tiger shrimp with tempura sauce</p>
-            <h4>RM23.90 + RM1.20(SST) = </h4>
-            <h3>RM25.10</h3>
-            <h4>[166 Kcal]</h4>
-          </div>
-
-          <div className="section__image" onClick={handleProductClick}>
-            <img src={Image3} alt="" />
-          </div>
-
-          <div className="section__description">
-            <h2>Shrimp Tempura</h2>
-            <p>5 tiger shrimp with tempura sauce</p>
-            <h4>RM23.90 + RM1.20(SST) = </h4>
-            <h3>RM25.10</h3>
-            <h4>[166 Kcal]</h4>
-          </div>
-
-          <div className="section__image" onClick={handleProductClick}>
-            <img src={Image3} alt="" />
-          </div>
-
-          <div className="section__description">
-            <h2>Shrimp Tempura</h2>
-            <p>5 tiger shrimp with tempura sauce</p>
-            <h4>RM23.90 + RM1.20(SST) = </h4>
-            <h3>RM25.10</h3>
-            <h4>[166 Kcal]</h4>
-          </div>
-
-          <div className="section__image" onClick={handleProductClick}>
-            <img src={Image3} alt="" />
-          </div>
-
-          <div className="section__description">
-            <h2>Shrimp Tempura</h2>
-            <p>5 tiger shrimp with tempura sauce</p>
-            <h4>RM23.90 + RM1.20(SST) = </h4>
-            <h3>RM25.10</h3>
-            <h4>[166 Kcal]</h4>
-          </div>
-
-          <div className="section__image" onClick={handleProductClick}>
-            <img src={Image3} alt="" />
-          </div>
+          {products.map((product) => (
+            <div key={product.code} className="section__description" onClick={handleProductClick}>
+              <h2>{product.name}</h2>
+              <p>{product.description}</p>
+              <h4>{`RM${product.price} + RM1.20(SST) = `}</h4>
+              <h3>{`RM${parseFloat(product.price) + 1.20}`}</h3>
+              <h4>[166 Kcal]</h4>
+            </div>
+          ))}
+          
         </div>
       </div>
 
