ce02ac5f256d4ec0169b4a0a2bf03afe6feb34cf
Daniel Evangelista <danicvan@hotmail.com>
2024-01-11 08:52:33 -0300

    feat: initialized middleware for authorization token and improved cart page to save the products added on the cart e to show in the screen

--- src/Pages/Cart/Cart.jsx
+++ src/Pages/Cart/Cart.jsx
@@ -1,23 +1,53 @@
+import { useEffect, useState } from "react";
+
+
 function Cart({ product }) {
+
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch('http://localhost:3000/cart');
+        const result = await response.json();
+        console.log(result);
+        setData(result);
+      } catch (error) {
+        console.error('Error fetching data: ', error);
+      } finally {
+        setLoading(false);
+      }
+    }
+
+    fetchData();
+  }, []);
+
   return (
     <>
-    <h1>Cart</h1>
-      {/* <div className="cart">
-        <div className="cart__item">
-          <div className="cart__item-img">
-            <img src={product.image} alt={product.name} />
+      <div className="cart">
+        {loading ? (
+          <p>Loading...</p>
+        ) : (
+          data && data.length > 0 ? (
+            data.map((item) => {
+              <div>
+                <p>{item.code}</p>
+              </div>
+            })
+          ) : (
+            <p>No data available.</p>
+          )
+        )}
+        {/* {data.map((product) => (
+          <div
+            key={product.code}
+            className="cart__item"
+          >
+            <h2>{product.description}</h2>
           </div>
-
-          <div className="cart__item-description">
-            <h2>{product.name}</h2>
-            <p>{product.description}</p>
-            <p>{`RM${parseFloat(product.price).toFixed(2)} + RM1.20(SST) = RM${(
-              parseFloat(product.price) + 1.2
-            ).toFixed(2)}`}</p>
-            <p>[166 Kcal]</p>
-          </div>
-        </div>
-      </div> */}
+        ))} */}
+      </div>
     </>
   )
 }
