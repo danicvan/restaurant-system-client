09c96f2d62eb592c0a6da966e336e989aba3db4f
Daniel Camilo <daniel.camilo@grupomfx.com>
2024-04-14 16:14:26 -0300

    feat: adjusted script to handle login page and permissions according users registered and customized layouts

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -11,7 +11,10 @@ function Login() {
 
     const handleLoginClick = async () => {
         try {
-            const response = await axios.post("/login", { username, password });
+            const response = await axios.post("http://localhost:3000/login", {
+                username,
+                password,
+            });
             const token = response.data.token;
             // Store token securely in local storage
             localStorage.setItem("token", token);
@@ -32,21 +35,23 @@ function Login() {
                             <img src={bigLogo} alt="" />
                         </div>
 
-                        <div className="main__login">
-                            <input
-                                type="text"
-                                placeholder="Username"
-                                value={username}
-                                onChange={(e) => setUsername(e.target.value)}
-                            />
-                            <input
-                                type="password"
-                                placeholder="Password"
-                                value={password}
-                                onChange={(e) => setPassword(e.target.value)}
-                            />
-                            <button onClick={handleLoginClick}>Login</button>
-                            {error && <div className="error">{error}</div>}
+                        <div className="main__credentials">
+                            <div className="main__login">
+                                <input
+                                    type="text"
+                                    placeholder="Username"
+                                    value={username}
+                                    onChange={(e) => setUsername(e.target.value)}
+                                />
+                                <input
+                                    type="password"
+                                    placeholder="Password"
+                                    value={password}
+                                    onChange={(e) => setPassword(e.target.value)}
+                                />
+                                <button onClick={handleLoginClick}>Login</button>
+                                {error && <div className="error">{error}</div>}
+                            </div>
                         </div>
                     </div>
                 </>
